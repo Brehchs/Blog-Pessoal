3f8b1ffc04016885f519abce6c2dbe29dd14e59e
Brenda Chagas <brenda.chsilva@hotmail.com>
2022-09-28 12:31:15 -0300

    Aplicando CSS simples no React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+    return(
+        <> 
+            <h1 className="titulo">Bem Vindo</h1> 
+            <img src="https://cdn.pixabay.com/photo/2013/10/25/17/26/tree-200795_960_720.jpg" alt="Desenho de uma árvore com várias tecnologias e redes sociais" className="img" />
+            
+        </>
+    );
+}
+
+export default Home;
