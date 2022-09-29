03603a78cfb7ce6c297b5a8166be20f63447d5d7
Brenda Chagas <brenda.chsilva@hotmail.com>
2022-09-29 13:13:07 -0300

    Aprendendo e testando aplicações do Material UI

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,12 +1,23 @@
 import React from "react";
+import {Grid, Paper, Button} from "@material-ui/core";
+import { Box } from '@mui/material';
 import "./Home.css";
 
 function Home() {
     return(
         <> 
-            <h1 className="titulo">Bem Vindo</h1> 
-            <img src="https://cdn.pixabay.com/photo/2013/10/25/17/26/tree-200795_960_720.jpg" alt="Desenho de uma árvore com várias tecnologias e redes sociais" className="img" />
-            
+            <Paper>
+                <Box p={2}>
+                    <Box display="flex" justifyContent="center">
+                        <h1>Título</h1>
+                    </Box>
+                    <img src="https://i.pinimg.com/originals/b3/2c/2d/b32c2d5cf17655fdbc61f66416ff19f0.jpg" alt="desenho de mulher negra com tranças" style={{width: "100%", height: "100%"}} />
+                    <Box display="flex" justifyContent="center" p={2}>
+                        <Button variant="contained"color="primary">Texto 1</Button>
+                        <Button variant="contained" color="secondary">Texto 2</Button>
+                    </Box>
+                </Box>
+            </Paper>
         </>
     );
 }
