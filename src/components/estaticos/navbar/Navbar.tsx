6f111835726905353323f17e2d5a152b25f9ae3d
Brenda Chagas <brenda.chsilva@hotmail.com>
2022-10-04 17:01:23 -0300

    Acrescentando Model e Service

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,56 +1,101 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography} from '@material-ui/core';
-import { createTheme } from "@mui/material/styles";
-import { cyan } from '@mui/material/colors';
-import { Box } from '@mui/material';
-import { Link } from 'react-router-dom';
-import "./Navbar.css";
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import Open from './Open';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }),
+);
 
 function Navbar() {
-    return (
-        <>
-            <AppBar position="static">
-                <Toolbar variant="dense">
-                    <Box className="cursor">
-                        <Typography variant="h5" color="inherit">
-                            Blog Pessoal 
-                        </Typography>
-                    </Box>
+  const classes = useStyles();
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
 
-                    <Box display="flex" justifyContent="start">
-                        <Box mx={1} className="cursor">
-                            <Typography variant="h6" color="inherit">
-                                Home 
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className="cursor">
-                            <Typography variant="h6" color="inherit">
-                                Postagens 
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className="cursor">
-                            <Typography variant="h6" color="inherit">
-                                Temas 
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className="cursor">
-                            <Typography variant="h6" color="inherit">
-                                Cadastrar Tema 
-                            </Typography>
-                        </Box>
-                        <Link to="/login" className="text-decorator-none">
-                            <Box mx={1} className="cursor">
-                                <Typography variant="h6" color="inherit">
-                                    Logout 
-                                </Typography>
-                            </Box>
-                        </Link>
-                    </Box>
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAuth(event.target.checked);
+  };
 
-                </Toolbar>
-            </AppBar>
-        </>
-    )
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className={classes.root}>
+      <FormGroup>
+        <FormControlLabel
+          control={<Switch checked={auth} onChange={handleChange} aria-label="login switch" />}
+          label={auth ? 'Logout' : 'Login'}
+        />
+      </FormGroup>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <Open />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Blog Pessoal
+          </Typography>
+          {auth && (
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Perfil</MenuItem>
+                <MenuItem onClick={handleClose}>Minha Conta</MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
 export default Navbar;
--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,40 +1,130 @@
-import React from "react";
-import { Grid, Box, TextField, Typography, Button } from '@mui/material';
-import {Link} from "react-router-dom";
+import React, { useState, useEffect, ChangeEvent } from "react";
+import { Grid, Box, TextField, Typography, Button } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+import useLocalStorage from "react-use-localstorage";
+import {login} from "../../services/Service";
+import UserLogin from "../../models/UserLogin";
 import "./Login.css";
 
-
 function Login() {
-    
-    return(
-        <Grid container direction="row" justifyContent="center" alignItems="center" className="corPagina">
-            <Grid alignItems="center" xs={6}>
-                <Box paddingX={20}>
-                    <form>
-                        <Typography variant="h3" gutterBottom  component="h3" align="center" className="textoslogin">Entrar</Typography>
-                        <TextField id="usuario" label="usuario" variant="outlined" name="usuario" margin="normal" fullWidth />
-                        <TextField id="senha" label="senha" variant="outlined" name="senha" margin="normal" type="password" fullWidth />
-                        <Box marginTop={2} textAlign="center">
-                        <Link to="/home" className="text-decorator-none">
-                        <Button className="botaologin"  type="submit" variant="contained">
-                            Logar
-                        </Button>
-                        </Link>
-                    </Box>
-                    </form>
-                    <Box display="flex" justifyContent="center" marginTop={2}>
-                        <Box marginRight={1}>
-                            <Typography variant="subtitle1" gutterBottom align="center" color="#F42B00">Não tem uma conta?</Typography>
-                        </Box>
-                        <Typography variant="subtitle2" gutterBottom align="center" fontSize={18} className="textoslogin">Cadastre-se</Typography>
-                    </Box>
-                </Box>
-            </Grid>
-            <Grid xs={6} className="imagem">
+  let navigate = useNavigate();
+  const [token, setToken] = useLocalStorage('token');
 
-            </Grid>
-        </Grid>
-    );
+  const [userLogin, setUserLogin] = useState <UserLogin>(
+    { // valores iniciais do State
+      id:0,
+      usuario:"",
+      senha:"",
+      token:""
+
+    }
+    )
+
+    function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+      setUserLogin({
+        ...userLogin,
+        [e.target.name]: e.target.value
+      })
+    }
+
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+      e.preventDefault(); // impede que o botão atualize a tela
+      try{
+        await login(`/usuario/logar`, userLogin, setToken)
+
+        alert("Usuário logado com sucesso!");
+      }catch(error){
+          alert("Dados do usuário inconsistentes. Erro ao logar!");
+      }
+    }
+
+    useEffect(() => {
+      if(token !== "") {
+        navigate("/home")
+      }
+    }, [token])
+
+  return (
+    <Grid
+      container
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      className="corPagina"
+    >
+      <Grid alignItems="center" xs={6}>
+        <Box paddingX={20}>
+          <form onSubmit={onSubmit}>
+            <Typography
+              variant="h3"
+              gutterBottom
+              component="h3"
+              align="center"
+              className="textoslogin"
+            >
+              Entrar
+            </Typography>
+            <TextField
+              value={userLogin.usuario}
+              onChange={(event: ChangeEvent<HTMLInputElement>) => updatedModel(event)}
+              id="usuario"
+              label="usuario"
+              variant="outlined"
+              name="usuario"
+              margin="normal"
+              fullWidth
+            />
+            <TextField
+              value={userLogin.senha}
+              onChange={(event: ChangeEvent<HTMLInputElement>) => updatedModel(event)}
+              id="senha"
+              label="senha"
+              variant="outlined"
+              name="senha"
+              margin="normal"
+              type="password"
+              fullWidth
+            />
+            <Box marginTop={2} textAlign="center">
+              
+                <Button
+                  className="botaologin"
+                  type="submit"
+                  variant="contained"
+                >
+                  Logar
+                </Button>
+             
+            </Box>
+          </form>
+          <Box display="flex" justifyContent="center" marginTop={2}>
+            <Box marginRight={1}>
+              <Typography
+                variant="subtitle1"
+                gutterBottom
+                align="center"
+                color="#F42B00"
+              >
+                Não tem uma conta?
+              </Typography>
+            </Box>
+            <Link to="/cadastrousuario" className="textoslogin ">
+              <Typography
+                variant="subtitle2"
+                gutterBottom
+                align="center"
+                fontSize={18}
+                className="textoslogin"
+              >
+                Cadastre-se
+              </Typography>
+            </Link>
+          </Box>
+        </Box>
+      </Grid>
+      <Grid xs={6} className="imagem"></Grid>
+    </Grid>
+  );
 }
 
 export default Login;
